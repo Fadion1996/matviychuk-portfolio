7003ef215f2d9306752d3392ae261970e99f6154
Vasiliy Yurkovets <yurkovets@Vasiliys-MacBook-Pro.local>
2020-02-06 23:51:51 +0200

    Updates

---
+++ precache-manifest.5451eb058582868a6114b0bbddb7eacb.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae2eb7ef8d56e85f48b60074f8f77b9",
+    "url": "/matviychuk-portfolio/index.html"
+  },
+  {
+    "revision": "c9bdd645ab19b05c0fb6",
+    "url": "/matviychuk-portfolio/static/css/main.70e35f4f.chunk.css"
+  },
+  {
+    "revision": "63b4fd2ce9c8d201164f",
+    "url": "/matviychuk-portfolio/static/js/2.8a373850.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/matviychuk-portfolio/static/js/2.8a373850.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9bdd645ab19b05c0fb6",
+    "url": "/matviychuk-portfolio/static/js/main.dd1d8f0a.chunk.js"
+  },
+  {
+    "revision": "3d0e6a4ee7c07a5d7515",
+    "url": "/matviychuk-portfolio/static/js/runtime-main.d6f2f5af.js"
+  },
+  {
+    "revision": "b41f567ec738828b109e0487050d2482",
+    "url": "/matviychuk-portfolio/static/media/1.b41f567e.jpg"
+  },
+  {
+    "revision": "2902169ee7af61664966d575a07c69c5",
+    "url": "/matviychuk-portfolio/static/media/10.2902169e.jpg"
+  },
+  {
+    "revision": "65b4b133b6889b152d38bdb2c6dc3b0a",
+    "url": "/matviychuk-portfolio/static/media/11.65b4b133.jpg"
+  },
+  {
+    "revision": "92ae599499a37127407ecb3530509e73",
+    "url": "/matviychuk-portfolio/static/media/12.92ae5994.jpg"
+  },
+  {
+    "revision": "bdb6101a3f592555f754a256c616d121",
+    "url": "/matviychuk-portfolio/static/media/13.bdb6101a.jpg"
+  },
+  {
+    "revision": "7d2c13babb5ec5962e1f04b44e00b05e",
+    "url": "/matviychuk-portfolio/static/media/2.7d2c13ba.jpg"
+  },
+  {
+    "revision": "a6f9c3c25a02db839c4c3f5fe8d695e5",
+    "url": "/matviychuk-portfolio/static/media/3.a6f9c3c2.jpg"
+  },
+  {
+    "revision": "e3b98e4b52ff112b1987bcd0b707c03e",
+    "url": "/matviychuk-portfolio/static/media/4.e3b98e4b.jpg"
+  },
+  {
+    "revision": "0364817b87e312270dc2adc120e8934c",
+    "url": "/matviychuk-portfolio/static/media/5.0364817b.jpg"
+  },
+  {
+    "revision": "04173392ac216cc78927bdc0f791f21f",
+    "url": "/matviychuk-portfolio/static/media/6.04173392.jpg"
+  },
+  {
+    "revision": "90ea91d8a179611ae8beeed928d34ce5",
+    "url": "/matviychuk-portfolio/static/media/7.90ea91d8.jpg"
+  },
+  {
+    "revision": "12e0b85d533282a67d3455d95f1e4145",
+    "url": "/matviychuk-portfolio/static/media/8.12e0b85d.jpg"
+  },
+  {
+    "revision": "259552df1519a4a6cf3e4165b05278e6",
+    "url": "/matviychuk-portfolio/static/media/9.259552df.jpg"
+  },
+  {
+    "revision": "f69c47e0d3b01741068b4dc6396aea5d",
+    "url": "/matviychuk-portfolio/static/media/nastia1.f69c47e0.jpg"
+  },
+  {
+    "revision": "eedc23ca9d8d20c264fdaccadd70e4c0",
+    "url": "/matviychuk-portfolio/static/media/nastia2.eedc23ca.jpg"
+  },
+  {
+    "revision": "445433867cc5de74eeab7e9ac4f4fab8",
+    "url": "/matviychuk-portfolio/static/media/nastia3.44543386.jpg"
+  },
+  {
+    "revision": "6e70b68f9f004ee1d9b5ce97c703bcb1",
+    "url": "/matviychuk-portfolio/static/media/nastia4.6e70b68f.jpg"
+  },
+  {
+    "revision": "ee62fed829b7842f315889d024b24d86",
+    "url": "/matviychuk-portfolio/static/media/nastia5.ee62fed8.jpg"
+  },
+  {
+    "revision": "5cb3cf1008bb30515d9020850d99897c",
+    "url": "/matviychuk-portfolio/static/media/nastia6.5cb3cf10.jpg"
+  },
+  {
+    "revision": "a4311f55c42e2cc9a2c8ecc0780c5cde",
+    "url": "/matviychuk-portfolio/static/media/nastia7.a4311f55.jpg"
+  },
+  {
+    "revision": "a0c79b628e080059654bab20a53c40fc",
+    "url": "/matviychuk-portfolio/static/media/texture.a0c79b62.png"
+  }
+]);
